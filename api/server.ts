f6c5089c2353cb712365662195bbf7de216d5f4a
Sreoshi Chakraborty <Iamsreoshi19@gmail.com>
2025-12-03 20:12:21 +0530

    chore: remove duplicate api/server.ts to avoid Vercel conflicts

--- api/server.ts
+++
@@ -1,9 +0,0 @@
-import serverless from "serverless-http";
-import { createApp } from "../server/app";
-
-// createApp is synchronous and returns an Express app
-const app = createApp();
-
-const handler = serverless(app);
-
-export default handler;
